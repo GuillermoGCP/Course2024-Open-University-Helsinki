6f1ef5a2e8b72c856fdbc4886a8f27ed5c66f1c9
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-10-17 19:26:32 +0200

    Exercise 1.7

--- part 1/unicafe/src/components/Statictis.jsx
+++ part 1/unicafe/src/components/Statictis.jsx
@@ -1,9 +1,23 @@
 const Statictis = ({ good, neutral, bad }) => {
+    const totalAverage = () => {
+        const total = good + neutral + bad
+        const points = good - bad
+        if (total === 0) return 0
+        return points / total
+    }
+    const positiveAverage = () => {
+        const total = good + neutral + bad
+        if (total === 0) return 0
+        return (good / total) * 100
+    }
     return (
         <>
             <p>{`Good: ${good}`}</p>
             <p>{`Neutral: ${neutral}`}</p>
             <p>{`Bad: ${bad}`}</p>
+            <p>{`All: ${bad + neutral + good}`}</p>
+            <p>{`Average: ${totalAverage()}`}</p>
+            <p>{`Positive: ${positiveAverage()} %`}</p>
         </>
     )
 }
