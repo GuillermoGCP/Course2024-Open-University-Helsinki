f52730485479ca93bb989c55c1042f467919e36f
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-10-18 16:05:45 +0200

    Reorder directories

---
+++ part 1/1-1.5/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import Part from './Part'
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            <Part part={parts[0].name} />
+            <Part part={parts[1].name} />
+            <Part part={parts[2].name} />
+        </>
+    )
+}
+export default Content
---
+++ part 1/1-1.5/src/components/Part.jsx
@@ -0,0 +1,8 @@
+const Part1 = ({ part }) => {
+    return (
+        <>
+            <p>{part}</p>
+        </>
+    )
+}
+export default Part1
